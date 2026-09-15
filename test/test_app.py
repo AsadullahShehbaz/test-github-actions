47d162a4356d69fc1cfbea9e53a47d4792f9630c
AsadullahShehbaz <asadullahcreative@mail.com>
2026-09-15 16:07:44 +0500

    hello world app

--- test/test_app.py
+++ test/test_app.py
@@ -1,6 +1,9 @@
-from src import app
+from src.app import app
 
 def test_hello():
-    response = app.test_client().get('/')
+    client = app.test_client()
+
+    response = client.get('/')
+
     assert response.status_code == 200
     assert response.data == b"Hello World!"
